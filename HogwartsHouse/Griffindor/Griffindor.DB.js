f017c8eeae481c2e01b792f33a0d4cb83017c815
soominnnn <marisol03@naver.com>
2024-03-28 17:16:21 +0900

    중복된 코드 수정

--- HogwartsHouse/Griffindor/Griffindor.DB.js
+++ HogwartsHouse/Griffindor/Griffindor.DB.js
@@ -1,46 +1,9 @@
 import { createDiv } from "../studentProfile.js";
-import {db, doc, setDoc, collection, storage, ref, getDownloadURL, uploadBytes} from '../../../core/firebase.js';
+import { uploadFile, createStudent} from "../uploadData.js"
 
-let imageURL = '';
 const dbRef = "Griffindor";
 
-document.querySelector('#file').addEventListener('change', function(){
-  let file = this.files[0];
-  const reader = new FileReader();
-
-  reader.onload = (event) => {
-    const studentImage = document.querySelector('.studentImage');
-    studentImage.style.backgroundImage = `url('${event.target.result}')`;
-  }
-  reader.readAsDataURL(file);
-
-  const storageRef = ref(storage, `${dbRef}/${file.name}`);
-
-  uploadBytes(storageRef, file).then((snapshot) => {
-    getDownloadURL(storageRef).then((downloadURL) => {
-      imageURL = downloadURL;
-    });
-  });
-})
-
-const addStudentButton = document.querySelector('.addStudentButton');
-
-async function createStudent() {
-  const studentName = document.querySelector('.nameInput').value;
-
-  if(studentName.length >= 13) {
-    alert("이름은 13자 이하로 작성해주세요.");
-  } else {
-    await setDoc(doc(collection(db, "Griffindor"), studentName), {
-      name: studentName, 
-      imageURL: imageURL
-    });
-  
-    location.reload();
-  }
-} 
-
-addStudentButton.addEventListener('click', createStudent);
-
+document.querySelector('#file').addEventListener('change', () => uploadFile(dbRef));
+document.querySelector('.addStudentButton').addEventListener('click', () => createStudent(dbRef));
 
 createDiv(dbRef);
